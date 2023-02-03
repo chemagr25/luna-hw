a75dbf3b71a035b521447cc7529f652618baead4
José María García Ramírez <chemagr25@gmail.com>
2023-02-02 22:10:13 -0600

    add vuetify3

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,39 @@
 <template>
-    <h1>Login</h1>
-</template>
+    <div class="text-center">
+      <v-chip
+        class="ma-2"
+      >
+        Default
+      </v-chip>
+
+      <v-chip
+        class="ma-2"
+        color="primary"
+      >
+        Primary
+      </v-chip>
+
+      <v-chip
+        class="ma-2"
+        color="secondary"
+      >
+        Secondary
+      </v-chip>
+
+      <v-chip
+        class="ma-2"
+        color="red"
+        text-color="white"
+      >
+        Red Chip
+      </v-chip>
+
+      <v-chip
+        class="ma-2"
+        color="green"
+        text-color="white"
+      >
+        Green Chip
+      </v-chip>
+    </div>
+  </template>
